6571544d5b9b3f27e996a843cb75ba3b4b8c3d7c
IvoSte <ivo.steegstra@gmail.com>
2023-05-17 10:35:28 +0200

    Voor de Leden

--- test.py
+++ test.py
@@ -2,5 +2,25 @@ def main():
     print("Hallo Leden!")
 
 
+def test():
+    print("Test")
+
+
+def fibonaci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        fibonaci(n - 1) + fibonaci(n - 2)
+
+
+# make gaussian
+def gaussian(x, mu, sigma):
+    return (
+        1 / (sigma * math.sqrt(2 * math.pi)) * math.exp(-0.5 * ((x - mu) / sigma) ** 2)
+    )
+
+
 if __name__ == "__main__":
     main()
